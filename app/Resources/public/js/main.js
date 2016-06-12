00bbe63dc16a38450fa145f5537a287c4a825fa1
S Langeweg <s.langeweg@bureaublauwgeel.nl>
2016-06-12 23:40:58 +0200

    Frontend update

--- app/Resources/public/js/main.js
+++ app/Resources/public/js/main.js
@@ -4,5 +4,11 @@ $(function () {
     $('[data-toggle="offcanvas"]').click(function () {
         $('.row-offcanvas').toggleClass('active')
     });
-    $('[data-toggle="popover"]').popover()
+    $('[data-toggle="popover"]').popover();
+    $('.button-collapse').sideNav({
+            menuWidth: 200, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+    );
 });
