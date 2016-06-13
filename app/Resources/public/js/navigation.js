85d8c5f0a9cff0beaae4186e9a714369aa0a64cc
S Langeweg <s.langeweg@bureaublauwgeel.nl>
2016-06-13 08:52:35 +0200

    Loading screen added for navigation. Issues with sidenav fixed.

--- app/Resources/public/js/navigation.js
+++ app/Resources/public/js/navigation.js
@@ -1,30 +1,44 @@
-document.addEventListener("DOMContentLoaded", function()
+document.addEventListener("DOMContentLoaded", function ()
 {
     Barba.Pjax.init();
 
     var FadeTransition = Barba.BaseTransition.extend({
-        start: function ()
-               {
-                   /**
-                    * This function is automatically called as soon the Transition starts
-                    * this.newContainerLoading is a Promise for the loading of the new container
-                    * (Barba.js also comes with an handy Promise polyfill!)
-                    */
+        start:       function ()
+                     {
+                         /**
+                          * This function is automatically called as soon the Transition starts
+                          * this.newContainerLoading is a Promise for the loading of the new container
+                          * (Barba.js also comes with an handy Promise polyfill!)
+                          */
 
-                   // As soon the loading is finished and the old page is faded out, let's fade the new page
-                   Promise
-                       .all([this.newContainerLoading, this.fadeOut()])
-                       .then(this.fadeIn.bind(this));
-               },
+                         // As soon the loading is finished and the old page is faded out, let's fade the new page
+                         Promise
+                             .all([this.showLoading(), this.newContainerLoading, this.fadeOut()])
+                             .then(this.fadeIn.bind(this))
+                             .then(this.hideLoading.bind(this));
+                     },
+        showLoading: function ()
+                     {
 
-        fadeOut: function ()
-                 {
-                     /**
-                      * this.oldContainer is the HTMLElement of the old Container
-                      */
+                         $('#loading-overlay').addClass('show');
 
-                     return $(this.oldContainer).animate({opacity: 0}).promise();
-                 },
+                     },
+        hideLoading: function ()
+                     {
+                         var _this = this;
+                         setTimeout(function()
+                         {
+                             $('#loading-overlay').removeClass('show');
+                         }, 400);
+                         _this.done();
+                     },
+        fadeOut:     function ()
+                     {
+                         /**
+                          * this.oldContainer is the HTMLElement of the old Container
+                          */
+                         return $(this.oldContainer).animate({opacity: 0}).promise();
+                     },
 
         fadeIn: function ()
                 {
@@ -34,8 +48,8 @@ document.addEventListener("DOMContentLoaded", function()
                      * Please note, newContainer is available just after newContainerLoading is resolved!
                      */
 
-                    var _this = this;
-                    var $el   = $(this.newContainer);
+
+                    var $el = $(this.newContainer);
 
                     $(this.oldContainer).hide();
 
@@ -51,8 +65,7 @@ document.addEventListener("DOMContentLoaded", function()
                          * .done() will automatically remove from the DOM the old Container
                          */
 
-                        _this.done();
-                    });
+                    }).promise();
                 }
     });
 
