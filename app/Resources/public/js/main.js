cee97324bd7d908ebb2de4a146c2e6f137027da8
S Langeweg <s.langeweg@bureaublauwgeel.nl>
2017-03-08 01:31:03 +0100

    Breadcrumbs and better play bar on scroll.

--- app/Resources/public/js/main.js
+++ app/Resources/public/js/main.js
@@ -8,17 +8,17 @@ $(function ()
       menuWidth: 200, // Default is 240
     }
   );
-  $('.backdrop > .info-bar').pushpin({
+  $('.backdrop  .info-bar').pushpin({
     top:    300,
     offset: 0
   });
   Barba.Dispatcher.on('transitionCompleted', function()
   {
-    $('.backdrop > .info-bar').pushpin({
+    $('.backdrop .info-bar').pushpin({
       top:    500,
       offset: 0
     });
   });
 
-  $('body').bootstrapMaterialDesign();
+  //$('body').bootstrapMaterialDesign();
 });
