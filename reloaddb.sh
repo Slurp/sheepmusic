b4e4efc3f7c104b428b95a0b1c09d485a1cdfe65
S Langeweg <s.langeweg@bureaublauwgeel.nl>
2017-03-20 20:59:26 +0100

    code cleanup

--- reloaddb.sh
+++ reloaddb.sh
@@ -3,5 +3,5 @@
 bin/console doc:data:drop --force
 bin/console doc:data:create
 bin/console doc:schema:update --force
-bin/console fos:user:create slurp slurpie@gmail.com warcraft3
+bin/console fos:user:create admin admin@gmail.com test
 bin/console music:import --env=dev -vvv
