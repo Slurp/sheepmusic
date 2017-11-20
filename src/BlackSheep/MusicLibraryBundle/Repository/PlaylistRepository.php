04b32b9891a955e29dd69c09cbb3a9129ec386f3
S Langeweg <s.langeweg@bureaublauwgeel.nl>
2017-11-20 09:22:18 +0100

    Rough playlist api
    Update the API for playlists.

--- src/BlackSheep/MusicLibraryBundle/Repository/PlaylistRepository.php
+++ src/BlackSheep/MusicLibraryBundle/Repository/PlaylistRepository.php
@@ -5,6 +5,7 @@ namespace BlackSheep\MusicLibraryBundle\Repository;
 use BlackSheep\MusicLibraryBundle\Entity\PlaylistEntity;
 use BlackSheep\MusicLibraryBundle\Model\Playlist;
 use BlackSheep\MusicLibraryBundle\Model\PlaylistInterface;
+use Doctrine\ORM\Query;
 
 /**
  * SongsRepository
@@ -24,5 +25,18 @@ class PlaylistRepository extends AbstractRepository implements PlaylistRepositor
         }
 
         return $playlist;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLists()
+    {
+        return $this->createQueryBuilder('a')->select(
+            ['a.id', 'a.name', 'a.createdAt', 'a.updatedAt','a.cover']
+        )->getQuery()->execute(
+            [],
+            Query::HYDRATE_ARRAY
+        );
     }
 }
