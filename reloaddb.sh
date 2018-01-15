2858c7f1f8a194f4e8bdb17affb0707488362995
S Langeweg <s.langeweg@bureaublauwgeel.nl>
2018-01-15 20:47:33 +0100

    Updated tooling

--- reloaddb.sh
+++ reloaddb.sh
@@ -4,4 +4,6 @@ bin/console doc:data:drop --force
 bin/console doc:data:create
 bin/console doc:schema:update --force
 bin/console fos:user:create admin admin@gmail.com test
-bin/console music:import --env=dev -vvv
+bin/console music:import
+bin/console library:update --env=prod
+bin/console fos:elastica:populate --env=prod
