b2604f2ece9bf915023f94a67df7e518a77762d2
Stephan Langeweg <slurpie@planet.nl>
2018-09-07 16:45:58 +0200

    git style deploy, see if that works.

---
+++ .travis/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    git config user.name "Travis CI"
+    git config user.email "slurpie+travisCI@gmail.com"
+
+   git config --global push.default matching
+   git remote add deploy ssh://git@$DEPLOY_HOST:$DEPLOY_DIR
+   git push deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi
+
