3e5351e9c1f33c3a0b24cbd04608ea6813ab0718
Stephan Langeweg <slurpie@gmail.com>
2018-09-27 20:20:14 +0200

    Updated shell scripts

--- shell/reloaddb.sh
+++ shell/reloaddb.sh
@@ -2,12 +2,12 @@
 
 rm -rf public/uploads
 mkdir -p public/uploads/_temp
-../bin/console ca:cl
-../bin/console ca:cl --env=prod
-../bin/console doc:data:drop --force
-../bin/console doc:data:create
-../bin/console doc:schema:update --force
-../bin/console fos:user:create admin admin@gmail.com test
-../bin/console music:import
-../bin/console library:update
-../bin/console fos:elastica:populate --env=prod
+bin/console ca:cl
+bin/console ca:cl --env=prod
+bin/console doc:data:drop --force
+bin/console doc:data:create
+bin/console doc:schema:update --force
+bin/console fos:user:create admin admin@gmail.com test
+bin/console music:import
+bin/console library:update
+bin/console fos:elastica:populate --env=prod
