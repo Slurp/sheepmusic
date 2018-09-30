d7d59614c847aaa435fe7d8175f81e70ac109c5a
Stephan Langeweg <slurpie@gmail.com>
2018-09-30 12:26:39 +0200

    Removed debugging statement

--- src/MusicLibrary/Factory/ArtworkFactory.php
+++ src/MusicLibrary/Factory/ArtworkFactory.php
@@ -107,13 +107,9 @@ class ArtworkFactory extends AbstractMediaFactory
                     $media = $this->getArtworkEntity($artworkCollection, $type);
                     $media->setLikes($artwork->likes);
                     $this->copyExternalFile($media, $artwork->url, $artworkCollection->getSlug() . '-' . $type);
-                    error_log("------");
-                    error_log($artwork->url);
-                    error_log($media->getImageFile());
                     if ($media->getImageFile() !== null) {
                         $artworkCollection->addArtwork($media);
                     }
-                    error_log("------");
                 }
             }
         }
